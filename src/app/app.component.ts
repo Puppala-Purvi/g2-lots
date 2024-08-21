5f9ff35eafa66ae9ad5f88d570894bbbe03d14ef
Purvi Puppala <purvi.puppala@copart.com>
2024-08-21 09:21:06 +0530

    rename

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import {LotsTypesComponent} from "./components/lots-types/lots-types.component";
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'g2-lots-types';
+  title = 'g2-lots';
 }
